48622045993935afd7a700de8653c77c361c6970
Brock Halladay <bhalladay@instructure.com>
2017-10-02 19:07:33 -0600

    promises exercise

---
+++ misc_scripts/promises.js
@@ -0,0 +1,16 @@
+let promiseWork = false;
+let somePromise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        if(promiseWork){
+            resolve('Hey, it worked!');
+        } else {
+            reject('Could not fulfill promise!');
+        }
+    }, 2000);
+});
+
+somePromise.then((message)=>{
+    console.log(`Success: ${message}`);
+}).catch((errorMessage)=>{
+    console.log(`Error: ${errorMessage}`);
+});
