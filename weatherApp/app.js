5c035c504a8d3bca9eddacee1fd43c083c573b60
Brock Halladay <bhalladay@instructure.com>
2017-10-04 15:10:52 -0600

    error handling for weatherApp

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -15,14 +15,21 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-// console.log(encodeURIComponent(argv.a));
 addressToFind = encodeURIComponent(argv.a);
 
 req({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressToFind}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if(error){
+    console.log(`Could not reach Google servers: ${error}`)
+  } else if (response.statusCode != "200") {
+    console.log(`Something went wrong with the API call, it returned with a ${response.statusCode} status code.`)
+  } else if(body.status.indexOf("ZERO_RESULTS") > -1){
+    console.log(`Invalid search. ${addressToFind} did not turn up any results or was ill formatted.`)
+  } else {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);  
+  }
 });
