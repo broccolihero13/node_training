57e02b453fe7510ff2d0bcd6a28d6ebbbc52ddd3
Brock Halladay <bhalladay@instructure.com>
2017-10-07 08:25:57 -0600

    finished axios test to use promise library for chaining calls together

--- weatherApp/axios_test.js
+++ weatherApp/axios_test.js
@@ -20,9 +20,16 @@ let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${ad
 axios.get(geocodeUrl).then((response)=> {
   if(response.data.status === "ZERO_RESULTS"){
     throw new Error('No results for API call, check to make sure your address is valid');
-  } else {
-    console.log(response.data);
   }
+    let lat = response.data.results[0].geometry.location.lat;
+    let lng = response.data.results[0].geometry.location.lng;
+    let weatherUrl = `https://api.darksky.net/forecast/b6682158d9c69bddad9e30fe31432dd7/${lat},${lng}`;
+    console.log(`Getting weather for ${response.data.results[0].formatted_address}`);
+    return axios.get(weatherUrl);
+}).then((response)=>{
+  let temperature = response.data.currently.temperature;
+  let appTemperature = response.data.currently.apparentTemperature;
+  console.log(`Right now, it is currently ${temperature} degress, but it feels like ${appTemperature}`);
 }).catch((e)=> {
   if(e.response.status == "404"){
     console.log("Unable to connect to API, check the URL");
