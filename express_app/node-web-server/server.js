845579df36d7f1042989478eabf3fc4f96166221
Brock Halladay <bhalladay@instructure.com>
2017-10-12 14:39:11 -0400

    updated handlers and return JSON responses and HTML responses

--- express_app/node-web-server/server.js
+++ express_app/node-web-server/server.js
@@ -6,4 +6,28 @@ app.get('/', (request, response)=>{
   response.send("Hello Express");
 });
 
+app.get('/brocktest', (request, response)=>{
+  response.send({
+    title: "Brock's Test Page",
+    hobby: "Web Development",
+    id: "p1",
+    body: {
+      "title": "Page 1",
+      "sub-title": "Hero's Beginning",
+      "Character": "Veggiman",
+      "started": false
+    },
+    begin: function(){
+      body.started = true;
+    }
+  });
+});
+
+app.get('/bad', (request, response)=>{
+  response.send({
+    title: null,
+    id: "NO ID FOUND",
+    body: {}
+  })
+});
 app.listen(3000);
