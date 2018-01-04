2869adce69e9dd51c6acc174d03013a9dac36da2
Brock Halladay <bhalladay@instructure.com>
2018-01-04 13:24:36 -0700

    Added materialize, several pages, and fs module

--- express_app/node-web-server/server.js
+++ express_app/node-web-server/server.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 let currentUser = "James Bond";
 
+//this allows partials or pieces of repeated markup
+hbs.registerPartials(`${__dirname}/views/partials`);
+//this sets handlebars as the default for views and ties it to the views directory
+//without it, none of the "app.get" calls would work.
 app.set("view engine", "hbs");
+
+//this is middleware to log the date, request, and the pathname
+app.use((req,res,next)=>{
+  let now = new Date().toString();
+  let log = `${now} ${req.method} ${req.url}`;
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err)=>{
+    if(err){
+      console.log(`${err} - unable to append to server.log`);
+    }
+  })
+  next();
+});
+
+//this is the maintenance middleware - uncomment lines 28-30 if under maintenance
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs');
+// })
+
+//this allows you to grab files from the public directory for the browser
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper("getCurrentYear", ()=>{
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper("screamIt", (fixedString)=>{
+  return fixedString.toUpperCase();
+})
 
 let homePageValues = {
   title: "Home Page",
-  year: new Date().getFullYear(),
   author: "Brock Halladay",
   welcomeMessage: `Hello, ${currentUser}, welcome to this express app`
 }
@@ -19,7 +51,6 @@ app.get('/', (request, response)=>{
 
 let aboutPageValues = {
   title: "About Page",
-  year: new Date().getFullYear(),
   author: "Brock Halladay"
 }
 app.get('/about', (request, response)=>{
