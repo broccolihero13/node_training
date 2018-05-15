c51b9746cac2dd4e6520c9e49600b7ff6bbac77d
Brock Halladay <bhalladay@instructure.com>
2018-05-15 17:09:34 -0600

    added user document through node

--- Section_7/node-todo-api/server/server.js
+++ Section_7/node-todo-api/server/server.js
@@ -5,26 +5,54 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 //save something new
 
-let Todo = mongoose.model('Todo', {
-  text: {
-    require: true,
+// let Todo = mongoose.model('Todo', {
+//   text: {
+//     required: true,
+//     type: String,
+//     minlength: 1,
+//     trim: true //get rid of any leading and trailing white space
+//   },
+//   status: {
+//     required: true,
+//     type: String,
+//     default: "Open"
+//   },
+//   completedAt: {
+//     type: Date,
+//     default: null
+//   }
+// });
+
+// let newTodo = new Todo({
+//   text: "        Yes     You      Can         ",
+// });
+
+// newTodo.save().then((msg)=>{
+//   console.log(`Saved Todo: ${msg}`);
+// }).catch((err)=>{console.log(err)});
+
+let User = mongoose.model('Users', {
+  username: {
+    required: true,
+    type: String,
+    minlength: 3,
+    trim: true
+  }, email: {
+    required: true,
+    trim: true,
+    minlength: 1,
     type: String
-  },
-  status: {
-    require: true,
+  }, password: {
+    required: false,
     type: String
-  },
-  completedAt: {
-    type: Date
   }
 });
 
-let newTodo = new Todo({
-  text: "Use all the things",
-  status: "In Progress",
-  completedAt: new Date()
+let newUser = new User({
+  username: "brock_o_lee",
+  email: "abc"
 });
 
-newTodo.save().then((msg)=>{
-  console.log(`Saved Todo: ${msg}`);
+newUser.save().then((msg)=>{
+  console.log(`Saved User ${msg}`);
 }).catch((err)=>{console.log(err)});
