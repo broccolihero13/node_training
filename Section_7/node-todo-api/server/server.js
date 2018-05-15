9b4b6e0dac312b7de7efa3e3a9fda22aa8adef85
Brock Halladay <bhalladay@instructure.com>
2018-05-15 16:48:04 -0600

    use mongo to create a collection and save a document

--- Section_7/node-todo-api/server/server.js
+++ Section_7/node-todo-api/server/server.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 //save something new
+
+let Todo = mongoose.model('Todo', {
+  text: {
+    require: true,
+    type: String
+  },
+  status: {
+    require: true,
+    type: String
+  },
+  completedAt: {
+    type: Date
+  }
+});
+
+let newTodo = new Todo({
+  text: "Use all the things",
+  status: "In Progress",
+  completedAt: new Date()
+});
+
+newTodo.save().then((msg)=>{
+  console.log(`Saved Todo: ${msg}`);
+}).catch((err)=>{console.log(err)});
