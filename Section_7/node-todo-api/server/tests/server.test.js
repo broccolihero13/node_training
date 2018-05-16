55729bb2ebc2ba50cfb51798438bcf9b5d678f5d
Brock Halladay <bhalladay@instructure.com>
2018-05-16 10:00:27 -0600

    added testing for GET API call

--- Section_7/node-todo-api/server/tests/server.test.js
+++ Section_7/node-todo-api/server/tests/server.test.js
@@ -5,8 +5,12 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
+const todos = [{text: 'First test todo'},{text: 'Second test todo'}];
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=> done());
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=> done());
 });
 
 describe('POST /todos', ()=>{
@@ -24,7 +28,7 @@ describe('POST /todos', ()=>{
       if(err){
         return done(err);
       }
-      Todo.find().then((allTodos)=>{
+      Todo.find({text}).then((allTodos)=>{
         expect(allTodos.length).toBe(1);
         expect(allTodos[0].text).toBe(text);
         done();
@@ -42,9 +46,27 @@ describe('POST /todos', ()=>{
           return done(err);
         }
         Todo.find().then((allTodos)=>{
-          expect(allTodos.length).toBe(0);
+          expect(allTodos.length).toBe(2);
           done();
         }).catch((err)=>done(err))
       });
   });
+
+  it('should fetch all todos', (done)=>{
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end((err,res)=>{
+        if(err){
+          return done(err);
+        }
+        Todo.find().then((allTodos)=>{
+          expect(allTodos.length).toBe(2);
+          done()
+        }).catch((err)=>done(err));
+      });
+  })
 })
