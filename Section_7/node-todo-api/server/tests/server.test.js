57f9697d5580979a4a629e20a782a9ee0637412d
Brock Halladay <bhalladay@instructure.com>
2018-05-21 10:17:13 -0600

    added tests for GET calls

--- Section_7/node-todo-api/server/tests/server.test.js
+++ Section_7/node-todo-api/server/tests/server.test.js
@@ -1,11 +1,20 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
-const todos = [{text: 'First test todo'},{text: 'Second test todo'}];
+const todos = [
+  {
+    text: 'First test todo', 
+    _id: new ObjectID()
+  },
+  {
+    text: 'Second test todo',
+    _id: new ObjectID()
+  }];
 
 beforeEach((done)=>{
   Todo.remove({}).then(()=>{
@@ -35,7 +44,6 @@ describe('POST /todos', ()=>{
       }).catch((err)=>done(err))
     });
   });
-
   it('should not create a to do and return a bad data response', (done)=>{
     request(app)
       .post('/todos')
@@ -51,7 +59,8 @@ describe('POST /todos', ()=>{
         }).catch((err)=>done(err))
       });
   });
-
+});
+describe('GET /todos', ()=>{
   it('should fetch all todos', (done)=>{
     request(app)
       .get('/todos')
@@ -68,5 +77,29 @@ describe('POST /todos', ()=>{
           done()
         }).catch((err)=>done(err));
       });
+  });
+});
+describe('GET /todos/:id', ()=>{
+  it('should return todo doc', (done)=>{
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(todos[0].text).toBe(res.body.todo.text);
+    })
+    .end(done);
+  });
+  it('should return a 404 if todo is not found', (done)=>{
+    let randID = new ObjectID();
+    request(app)
+    .get(`/todos/${randID.toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+  it('ashould return a 404 for non-object ids', (done)=>{
+    request(app)
+    .get(`/todos/abcdefg`)
+    .expect(404)
+    .end(done);
   })
-})
+});
